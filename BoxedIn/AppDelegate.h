e049df8c6947f6085fccc1f72920bda8e228d804
rjmiller2543 <MBA11>
2015-10-19 08:49:37 -0400

    Initial Commit

---
+++ BoxedIn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BoxedIn
+//
+//  Created by Robert Miller on 10/19/15.
+//  Copyright (c) 2015 Robert Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BoxedIn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoxedIn
+//
+//  Created by Robert Miller on 10/19/15.
+//  Copyright (c) 2015 Robert Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
